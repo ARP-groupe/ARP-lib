efec21367466a57bd6b498cf8d25c4bec22a0e00
Arturo Ruiz Pascual <arturoruizpascual@gmail.com>
2025-02-25 23:19:24 +0100

    Update template.dart

--- lib/src/ui/widgets/template.dart
+++ lib/src/ui/widgets/template.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_form_bloc/flutter_form_bloc.dart';
 import 'package:provider/single_child_widget.dart';
+import 'package:rxdart/rxdart.dart';
 
 final modalController = BehaviorSubject<Widget>()..sink.add(const SizedBox());
 class TemplateWidget extends StatefulWidget {
