53957dea5706f6ae434078a5f0fb3e12d69a31b9
DrylockRuiz <drylock.ruiz@gmail.com>
2025-12-31 09:33:23 +0100

    Enhance exception handling in custom_exception.dart
    Added a safeCall method for handling exceptions and improved CustomException with additional logging.

--- lib/src/domain/exceptions/custom_exception.dart
+++ lib/src/domain/exceptions/custom_exception.dart
@@ -1,8 +1,51 @@
-class RepositoryException implements Exception {
-  final String message;
-  final dynamic originalError;
-  RepositoryException(this.message, [this.originalError]);
-  
-  @override
-  String toString() => 'RepositoryException: $message ($originalError)';
+
+import 'package:arp_lib/src/app.dart';
+import 'package:http/http.dart' as http;
+
+
+
+  Future<T> safeCall<T>(
+    String name, 
+    Future<T> Function() function, 
+  ) async {
+  try {
+    return await function();
+  } on CustomException {
+    rethrow; // Si ya es una CustomException, no la envuelvas de nuevo
+  } on http.ClientException catch (e) {
+    throw CustomException(cause: 'Network error in $name: ${e.message}');
+  } on FormatException catch (e) {
+    throw CustomException(cause: 'JSON format error in $name', originalError: e);
+  } catch (e) {
+    throw CustomException(
+      cause: 'Unexpected error in $name',
+      originalError: e,
+    );
+  }
 }
+
+
+class CustomException implements Exception {
+  final String cause;
+  final Function? function;
+  final dynamic originalError; // Para depuración técnica
+
+  CustomException({
+    required this.cause,
+    this.function,
+    this.originalError,
+  }) {
+    String logMsg = '${DateTime.now()} $runtimeType: $cause';
+    if (originalError != null) logMsg += ' | Original: $originalError';
+    
+    logger.severe(logMsg);
+
+    if (function != null) {
+      function!();
+    }
+  }
+
+  @override
+  String toString() => cause;
+}
+
